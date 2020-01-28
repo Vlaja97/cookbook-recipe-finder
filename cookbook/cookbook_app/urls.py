dbaffcfc4cf34fb6a516b1291f35d588cf4f369c
Vlada vlaa <v.vlajic.vlada@gmail.com>
2020-01-27 21:08:32 -0800

    applying bootstrap to frontend

--- cookbook/cookbook_app/urls.py
+++ cookbook/cookbook_app/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views import New_recipe
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', views.base, name='cookbook-app-base'),
-    path('home/', views.home, name='cookbook_app-home'),
     path('about/', views.about, name='cookbook_app-about'),
-     path('new_recipe/', New_recipe.as_view(), name='cookbook_app-new_recipe'),
+    path('new_recipe/', New_recipe.as_view(), name='cookbook_app-new_recipe'),
 ]
+
+urlpatterns += staticfiles_urlpatterns()

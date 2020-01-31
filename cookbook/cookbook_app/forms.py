32f9fe9e0f0d555a4689dd007b37d9b0a6a38c28
Vlada vlaa <v.vlajic.vlada@gmail.com>
2020-01-30 22:27:15 -0800

    removed create_profile template and NameForm

--- cookbook/cookbook_app/forms.py
+++ cookbook/cookbook_app/forms.py
@@ -3,10 +3,9 @@ from .models import Recipe
 
 class RecipeForm(forms.ModelForm):
     name = forms.CharField(max_length=100)
+    file = forms.FileField()
      
     class Meta:
         model = Recipe
-        fields = ('name',)
+        fields = ('name','file')
 
-class NameForm(forms.Form):
-    your_name = forms.CharField(label='Your name', max_length=100)
